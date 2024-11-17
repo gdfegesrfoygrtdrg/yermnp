e903ce5b6e8ea91daac4fa39e465d7e98568d337
gdfegesrfoygrtdrg <167984452+gdfegesrfoygrtdrg@users.noreply.github.com>
2024-11-17 13:19:49 +0400

    Create Program.cs

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+class Program
+{
+    static void Main()
+    {
+        int count = 0;
+        for (int n = 0; n <= 365 / 28; n++)
+        {
+            for (int k = 0; k <= (365 - 28 * n) / 30; k++)
+            {
+                for (int m = 0; m <= (365 - 28 * n - 30 * k) / 31; m++)
+                {
+                    if (28 * n + 30 * k + 31 * m == 365)
+                    {
+                        Console.WriteLine($"Решение: (n={n},k={k},m={m})");
+                        count++;
+                    }
+                }
+            }
+        }
+    }
+}
+
+
+            
+
+
+
+
+
